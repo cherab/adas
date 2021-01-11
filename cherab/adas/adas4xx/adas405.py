Fixed unit conversion in adas405 interface.

cherab/adas/adas4xx/adas405.py
@@ -17,12 +17,14 @@
 # under the Licence.
 
 import numpy as np
+
+from cherab.core.utility import Cm3ToM3, PerCm3ToPerM3
 from adas import run_adas405 as _run_adas405
 
 
 _cached_adas405_calls = {}
 
-_electron_densities = [10**x * 1E6 for x in np.linspace(7.7, 15.3, num=24)]
+_electron_densities = [PerCm3ToPerM3.to(10**x) for x in np.linspace(7.7, 15.3, num=24)]
 _electron_temperatures = [10**x for x in np.linspace(-0.7, 4.2, num=100)]
 
 
@@ -38,7 +40,7 @@ def run_adas405(uid='adas', year=96, elem='ne'):
         te_list = []
         for ne in _electron_densities:
             for te in _electron_temperatures:
-                ne_list.append(ne * 1E-6)
+                ne_list.append(Cm3ToM3.to(ne))
                 te_list.append(te)
 
         fraction, power = _run_adas405(uid=uid, year=year, elem=elem, te=te_list, dens=ne_list)
@@ -47,11 +49,11 @@ def run_adas405(uid='adas', year=96, elem='ne'):
         number_species = fractional_array.shape[-1]
         fractional_array = fractional_array.reshape((len(_electron_densities), len(_electron_temperatures), number_species))
 
-        total_power_array = power['total'].reshape((len(_electron_densities), len(_electron_temperatures))) * 1E6
-        plt_array = power['plt'].reshape((len(_electron_densities), len(_electron_temperatures))) * 1E6
-        prb_array = power['prb'].reshape((len(_electron_densities), len(_electron_temperatures))) * 1E6
-        prc_array = power['prc'].reshape((len(_electron_densities), len(_electron_temperatures))) * 1E6
-        stage_resolved_line_radiation = power['ion'].reshape((len(_electron_densities), len(_electron_temperatures), number_species)) * 1E6
+        total_power_array = Cm3ToM3.to(power['total'].reshape((len(_electron_densities), len(_electron_temperatures))))
+        plt_array = Cm3ToM3.to(power['plt'].reshape((len(_electron_densities), len(_electron_temperatures))))
+        prb_array = Cm3ToM3.to(power['prb'].reshape((len(_electron_densities), len(_electron_temperatures))))
+        prc_array = Cm3ToM3.to(power['prc'].reshape((len(_electron_densities), len(_electron_temperatures))))
+        stage_resolved_line_radiation = Cm3ToM3.to(power['ion'].reshape((len(_electron_densities), len(_electron_temperatures), number_species)))
 
         _cached_adas405_calls[unique_cache_id] = (fractional_array, total_power_array, plt_array, prb_array, prc_array, stage_resolved_line_radiation)
 